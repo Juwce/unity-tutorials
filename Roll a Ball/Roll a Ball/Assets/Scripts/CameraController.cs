f45672ffb3d35d99719157c5c2f37f94cf7ea4f4
Tim Hedstrom <tim@tims-mbp.attlocal.net>
2017-10-26 11:32:39 -0700

    added tutorials

---
+++ Roll a Ball/Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = offset = transform.position - player.transform.position;
+	}
+	
+	// Use late update so we know absolutely that the player has moved for this frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
